a702e070e8e04f5266feecf1de325d39fdefaa2f
Gor SHAROYAN <Gor.SHAROYAN95@gmail.com>
2025-12-08 09:22:33 +0000

    change image component in hero image it must ajust to image size

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,25 +1,21 @@
 'use client';
 
 import Image from 'next/image';
-import { PlaceHolderImages } from '@/lib/placeholder-images';
 import { useLanguage } from '@/hooks/use-language';
 
 export function Hero() {
   const { t } = useLanguage();
-  const heroImage = PlaceHolderImages.find(img => img.id === 'hero-background');
 
   return (
-    <section className="relative w-full h-[100vh] text-white">
-      {heroImage && (
+    <section className="relative w-full text-white">
         <Image
-          src={heroImage.imageUrl}
-          alt={heroImage.description}
-          fill
-          className="object-cover"
-          data-ai-hint={heroImage.imageHint}
-          priority
+            src="/assets/solution.am_background.jpg"
+            alt="Solution.am Background"
+            width={1920}
+            height={1080}
+            className="object-contain w-full h-auto"
+            priority
         />
-      )}
       <div className="absolute inset-0 bg-black/60 flex flex-col items-center justify-center text-center p-4">
         <h1 className="font-headline text-4xl font-bold tracking-tight md:text-6xl">
           {t('home.hero.title')}
