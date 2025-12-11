42fc9e5c8b1a948929277ceb1ce7633fcf8c9062
Zen 0ne <gor.sharoyan95@gmail.com>
2025-12-11 19:18:38 +0400

    :construction_worker:build : added some additional features in next.config.ts in order to enable static import to firebase

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type {NextConfig} from 'next';
 
+
+
+
+
 const nextConfig: NextConfig = {
+   output: 'export', // Enables static HTML export
+  trailingSlash: true, // Optional: ensures proper routing for Firebase
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
